3255bb2d9f768678d109e9af46fb90dfb0a3124f
madhu <madhu45708@gmail.com>
2026-05-30 15:13:12 +0530

    Fix database connection

--- db.py
+++ db.py
@@ -2,14 +2,10 @@ import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-
 def get_conn():
+    print("DATABASE_URL =", os.getenv("DATABASE_URL"))
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT", "5432"),
+        os.getenv("DATABASE_URL"),
         cursor_factory=RealDictCursor
     )
 
