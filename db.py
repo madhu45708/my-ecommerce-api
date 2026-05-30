9d00477c144634f0b758c3cae2002f6115dde696
madhu <madhu45708@gmail.com>
2026-05-30 11:55:04 +0530

    Fix db connection

--- db.py
+++ db.py
@@ -1,23 +1,16 @@
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 def get_conn():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        database=os.getenv("DB_NAME", "E_commerce"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "root"),
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
         port=os.getenv("DB_PORT", "5432"),
-        cursor_factory=RealDictCursor,
+        cursor_factory=RealDictCursor
     )
-
-
-conn = get_conn()
-cursor = conn.cursor()
 
 TIMEZONE = os.getenv("TIMEZONE", "Asia/Kolkata")
